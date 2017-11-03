1345bb09905b74a434ffb0771bb7d5c585e5d0bd
developer119 <developer119korea@gmail.com>
2017-11-03 13:54:07 +0900

    스크롤뷰 콘텐츠를 재사용하도록 수정

--- Assets/Scripts/ScrollViewDev.cs
+++ Assets/Scripts/ScrollViewDev.cs
@@ -10,10 +10,17 @@ public class ScrollViewDev : MonoBehaviour
     public RectTransform contentPanelRT_ = null;
     public GameObject baseContent_ = null;
     public Vector2 contentSpacing_ = Vector2.zero;
-
+    public int dataCount_ = 0;
     public int contentsCount_ = 0;
 
-    private Content[] _contents = null;
+    private List<Content> _contents = new List<Content>();
+    private Vector2 contentOverallSize = Vector2.zero; // 객체 하나에 필요한 사이즈
+    private Vector2 _contentsTotalSize = Vector2.zero; // 객체들 모두합한 사이즈
+    private int _contentColumn;
+    private int _contentRow;
+    private int _dataRow;
+    private int _currentScrollCount;
+    private int _previousScrollCount = 0;
 
     public void Awake()
     {
@@ -22,40 +29,133 @@ public class ScrollViewDev : MonoBehaviour
 
     public void Start()
     {
+        UpdateContent();
+    }
 
+    public void UpdateContentPosition(Vector2 vec)
+    {
+        _currentScrollCount = (int)(contentPanelRT_.anchoredPosition.y / contentOverallSize.y);
+
+        if (_currentScrollCount == _previousScrollCount || _currentScrollCount < 0 || _currentScrollCount > (_dataRow - _contentRow))
+        {
+            return;
+        }
+
+        if (_currentScrollCount > _previousScrollCount)
+        {
+            RepositionDown();
+        }
+        else if (_currentScrollCount < _previousScrollCount)
+        {
+            RepositionUp();
+        }
+        _previousScrollCount = _currentScrollCount;
+    }
+
+    private void RepositionDown()
+    {
+        List<Content> firstRowList = new List<Content>();
+        for (int i = 0; i < _contentColumn; i++)
+        {
+            firstRowList.Add(_contents[i]);
+        }
+        for (int i = 0; i < _contentColumn; i++)
+        {
+            _contents.Remove(firstRowList[i]);
+        }
+        for (int i = 0; i < firstRowList.Count; i++)
+        {
+            firstRowList[i].UpdateContent(((_currentScrollCount * _contentColumn) + _contents.Count + i).ToString());
+        }
+        _contents.AddRange(firstRowList);
+
+        Vector3 newPos = Vector3.zero;
+        Transform targetTransform = null;
+
+        for (int i = 0; i < firstRowList.Count; i++)
+        {
+            targetTransform = firstRowList[i].transform;
+            newPos = new Vector3(targetTransform.localPosition.x, targetTransform.localPosition.y - _contentsTotalSize.y, targetTransform.localPosition.z);
+            targetTransform.localPosition = newPos;
+        }
+    }
+
+    private void RepositionUp()
+    {
+        List<Content> lastRowList = new List<Content>();
+        for (int i = _contents.Count - _contentColumn; i < _contents.Count; i++)
+        {
+            lastRowList.Add(_contents[i]);
+        }
+        for (int i = 0; i < lastRowList.Count; i++)
+        {
+            _contents.Remove(lastRowList[i]);
+        }
+        for (int i = 0; i < lastRowList.Count; i++)
+        {
+            lastRowList[i].UpdateContent(((_currentScrollCount * _contentColumn) + i).ToString());
+        }
+        _contents.InsertRange(0, lastRowList);
+
+        Vector3 newPos = Vector3.zero;
+        Transform targetTransform = null;
+        for (int i = 0; i < lastRowList.Count; i++)
+        {
+            targetTransform = lastRowList[i].transform;
+            newPos = new Vector3(targetTransform.localPosition.x, targetTransform.localPosition.y + _contentsTotalSize.y, targetTransform.localPosition.z);
+            targetTransform.localPosition = newPos;
+        }
+    }
+
+    private void UpdateContent()
+    {
+        for (int i = 0; i < _contents.Count; i++)
+        {
+            _contents[i].UpdateContent(i.ToString());
+        }
     }
 
     private void Initialize(GameObject go, Transform parent)
     {
-        _contents = new Content[contentsCount_];
         Vector2 spacing = contentSpacing_;
         Vector2 startPos = Vector2.zero;
         Vector2 contentPanelSize = Vector2.zero;
         Vector2 contentSize = baseContent_.GetComponent<RectTransform>().sizeDelta;
-        Vector3 totalSize = contentSize + spacing;
+        contentOverallSize = contentSize + spacing;
         Vector3 pos = Vector3.zero;
 
         float newPosX = 0.0f;
         float newPosY = 0.0f;
-        int column = (int)(scrollRT_.sizeDelta.x / totalSize.x);
-        int row = _contents.Length / column;
-        if (_contents.Length % column != 0)
+        _contentColumn = (int)(scrollRT_.sizeDelta.x / contentOverallSize.x);
+        _contentRow = contentsCount_ / _contentColumn;
+        _dataRow = dataCount_ / _contentColumn;
+
+        if (contentsCount_ % _contentColumn != 0)
         {
-            row++;
+            contentsCount_ += contentsCount_ % _contentColumn;
+            _contentRow++;
         }
-        startPos = new Vector2(spacing.x + ((scrollRT_.sizeDelta.x - (totalSize.x * column)) * 0.5f), -spacing.y);
-        contentPanelSize = new Vector2(contentPanelRT_.sizeDelta.x, totalSize.y * row); //@TODO: 리포지션으로 사용시에 수정필요
-        contentPanelRT_.sizeDelta = contentPanelSize;
-        for (int i = 0; i < _contents.Length; i++)
+        if (dataCount_ % _contentColumn != 0)
         {
-            newPosX = startPos.x + (totalSize.x * (i % column));
-            newPosY = startPos.y - (totalSize.y * (i / column));
+            _dataRow++;
+        }
+
+        _contentsTotalSize = new Vector2(contentOverallSize.x * _contentColumn, contentOverallSize.y * _contentRow);
+
+        startPos = new Vector2(spacing.x + ((scrollRT_.sizeDelta.x - (contentOverallSize.x * _contentColumn)) * 0.5f), -spacing.y);
+        contentPanelSize = new Vector2(scrollRT_.sizeDelta.x, contentOverallSize.y * _dataRow);
+        contentPanelRT_.sizeDelta = contentPanelSize;
+        for (int i = 0; i < contentsCount_; i++)
+        {
+            newPosX = startPos.x + (contentOverallSize.x * (i % _contentColumn));
+            newPosY = startPos.y - (contentOverallSize.y * (i / _contentColumn));
             pos = new Vector3(newPosX, newPosY, 0);
 
             GameObject obj = Instantiate(go, parent) as GameObject;
             obj.transform.localPosition = pos;
             obj.transform.localRotation = Quaternion.identity;
             obj.transform.localScale = Vector3.one;
+            _contents.Add(obj.GetComponent<Content>());
         }
     }
 }
